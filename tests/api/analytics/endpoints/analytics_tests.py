86277b915aad6f69cc193f55059758755a12f168
Sushma Ravichandran <sushma.ravichandran@ibm.com>
2020-01-14 15:38:21 -0500

    pbr test cases

--- tests/api/analytics/endpoints/analytics_tests.py
+++ tests/api/analytics/endpoints/analytics_tests.py
@@ -1144,14 +1144,12 @@ class TestAnalyticsCheckAndIncrementAPI(unittest.TestCase):
                 "baseline": {
                 "start_time": "2019-05-01T19:00:02.389Z",
                 "tags": {
-                    "destination_service_name": "reviews-v2",
                     "destination_workload": "reviews-v1"
                     }
                 },
                 "candidate": {
                 "start_time": "2019-05-01T19:00:02.389Z",
                 "tags": {
-                    "destination_service_name": "reviews-v2",
                     "destination_workload": "reviews-v3"
                     }
                 },
@@ -1178,12 +1176,204 @@ class TestAnalyticsCheckAndIncrementAPI(unittest.TestCase):
 
             #Call the REST API via the test client
             resp = self.flask_test.post(endpoint, json=parameters)
-            log.info(f"{resp.data}")
             self.assertEqual(resp.status_code, 200, resp.data)
 
-            ##################
-            # Test request to see if min max is not provided, if confidence is not provided
-            # Test to see if required=False parameters are not provided
-            # check if default sample size of 0 is filled for BR
-            #
+
             ###################
+            # Test request to check for default parameters
+            ###################
+            log.info("\n\n\n")
+            log.info('===TESTING ENDPOINT {endpoint}'.format(endpoint=endpoint))
+            log.info("Test request with some required parameters")
+
+            parameters = {
+                "baseline": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_workload": "reviews-v1"
+                    }
+                },
+                "candidate": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_workload": "reviews-v3"
+                    }
+                },
+                "traffic_control": {
+                   "success_criteria": [
+                   {
+                       "metric_name": "iter8_error_rate",
+                       "is_counter": False,
+                        "metric_query_template": "sum(increase(istio_requests_total{response_code=~\"5..\",reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "metric_sample_size_query_template": "sum(increase(istio_requests_total{reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "type": "threshold",
+                        "value": 0.02
+                        }
+                    ]
+                }
+                }
+
+            #Call the REST API via the test client
+            resp = self.flask_test.post(endpoint, json=parameters)
+            self.assertEqual(resp.status_code, 200, resp.data)
+
+            ###################
+            # Test request to with stop on failure=True
+            ###################
+            log.info("\n\n\n")
+            log.info('===TESTING ENDPOINT {endpoint}'.format(endpoint=endpoint))
+            log.info("Test request with some required parameters")
+
+            parameters = {
+                "baseline": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_service_namespace": "default",
+                    "destination_workload": "reviews-v1"
+                    }
+                },
+                "candidate": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_service_namespace": "default",
+                    "destination_workload": "reviews-v3"
+                    }
+                },
+                "traffic_control": {
+                   "success_criteria": [
+                   {
+                       "metric_name": "iter8_error_rate",
+                       "is_counter": False,
+                        "metric_query_template": "sum(increase(istio_requests_total{response_code=~\"5..\",reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "metric_sample_size_query_template": "sum(increase(istio_requests_total{reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "type": "threshold",
+                        "value": 19,
+                        "stop_on_failure": True
+                        }
+                    ]
+                }
+                }
+
+            #Call the REST API via the test client
+            resp = self.flask_test.post(endpoint, json=parameters)
+            correct_response = ["The experiment needs to be aborted", "All success criteria were not met", "Required confidence of 0.95 was not reached"]
+            self.assertEqual(correct_response, resp.get_json()["assessment"]["summary"]["conclusions"])
+            self.assertEqual(resp.status_code, 200, resp.data)
+
+
+    def test_payload_bayesian_routing_high_sample_size(self):
+        """Tests the REST endpoint /analytics/canary/posterior_bayesian_routing."""
+
+        endpoint = f'http://localhost:5555/api/v1/analytics/canary/posterior_bayesian_routing'
+
+        with requests_mock.mock() as m:
+            m.get(self.metrics_endpoint, json=json.load(open("tests/data/prometheus_sample_response_br.json")))
+
+            ###################
+            # Test request with high sample size for high confidence results
+            ###################
+            log.info("\n\n\n")
+            log.info('===TESTING ENDPOINT {endpoint}'.format(endpoint=endpoint))
+            log.info("Test request with some required parameters")
+
+            parameters = {
+                "baseline": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_workload": "reviews-v1"
+                    }
+                },
+                "candidate": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_workload": "reviews-v3"
+                    }
+                },
+                "traffic_control": {
+                   "confidence": 0.5,
+                   "success_criteria": [
+                   {
+                       "metric_name": "iter8_error_rate",
+                       "is_counter": False,
+                       "absent_value": "0",
+                       "min_max": {
+                           "min": 0,
+                           "max": 1
+                        },
+                        "metric_query_template": "sum(increase(istio_requests_total{response_code=~\"5..\",reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "metric_sample_size_query_template": "sum(increase(istio_requests_total{reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "type": "threshold",
+                        "value": 200000,
+                        "stop_on_failure": False
+                        }
+                    ]
+                }
+                }
+
+            #Call the REST API via the test client
+            resp = self.flask_test.post(endpoint, json=parameters)
+            self.assertEqual(resp.status_code, 200, resp.data)
+            correct_response = ["All success criteria were  met", "Required confidence of 0.5 was reached"]
+            self.assertEqual(correct_response, resp.get_json()["assessment"]["summary"]["conclusions"])
+
+
+            ###################
+            # Test request with high sample size for high confidence results + multiple metrics
+            ###################
+            log.info("\n\n\n")
+            log.info('===TESTING ENDPOINT {endpoint}'.format(endpoint=endpoint))
+            log.info("Test request with some required parameters")
+
+            parameters = {
+                "baseline": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_workload": "reviews-v1"
+                    }
+                },
+                "candidate": {
+                "start_time": "2019-05-01T19:00:02.389Z",
+                "tags": {
+                    "destination_workload": "reviews-v3"
+                    }
+                },
+                "traffic_control": {
+                   "confidence": 0.5,
+                   "success_criteria": [
+                   {
+                       "metric_name": "iter8_error_rate",
+                       "is_counter": False,
+                       "absent_value": "0",
+                       "min_max": {
+                           "min": 0,
+                           "max": 1
+                        },
+                        "metric_query_template": "sum(increase(istio_requests_total{response_code=~\"5..\",reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "metric_sample_size_query_template": "sum(increase(istio_requests_total{reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                        "type": "threshold",
+                        "value": 200000,
+                        "stop_on_failure": False
+                        },
+                    {
+                        "metric_name": "iter8_error_count",
+                        "is_counter": True,
+                        "absent_value": "0",
+                        "min_max": {
+                            "min": 0,
+                            "max": 1
+                         },
+                         "metric_query_template": "sum(increase(istio_requests_total{response_code=~\"5..\",reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                         "metric_sample_size_query_template": "sum(increase(istio_requests_total{reporter=\"source\"}[$interval]$offset_str)) by ($entity_labels)",
+                         "type": "threshold",
+                         "value": 200000,
+                         "stop_on_failure": False
+                         }
+                    ]
+                }
+                }
+
+            #Call the REST API via the test client
+            resp = self.flask_test.post(endpoint, json=parameters)
+            self.assertEqual(resp.status_code, 200, resp.data)
+            correct_response = ["All success criteria were  met", "Required confidence of 0.5 was reached"]
+            self.assertEqual(correct_response, resp.get_json()["assessment"]["summary"]["conclusions"])
