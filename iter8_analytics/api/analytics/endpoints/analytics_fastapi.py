f7af6b0cd7f60022154c24dec0d18e0415961080
Srinivasan Parthasarathy <spartha@us.ibm.com>
2020-04-03 17:27:43 -0400

    v1.0.0 input  and outputs

--- iter8_analytics/api/analytics/endpoints/analytics_fastapi.py
+++ iter8_analytics/api/analytics/endpoints/analytics_fastapi.py
@@ -1,118 +1,149 @@
-from fastapi import FastAPI
-from iter8_analytics.api.analytics.response_hil import Response
+# Module dependencies
+from fastapi import FastAPI, Body
+
+# iter8 stuff
+from iter8_analytics.api.analytics.response_hil import *
+from iter8_analytics.api.analytics.request_parameters_hil import *
+
 app = FastAPI()
 
-
-@app.post("/assessment", response_model=Response)
-def post():
-    resp = {
-    "versions": [
-    {
-      "id": "baseline",
-      "win_probability": 0.1,
-      "request_count": 500,
-      "metrics": [
+# An example for swagger documentation
+eip_example = {
+    'start_time': "2020-04-03T12:55:50.568Z",
+    "iter8_metrics": [
         {
-          "id": "iter8-latency",
-          "statistics": {
-            "sample_size": 500,
-            "value": 0.005,
-            "probability_to_be_best_version": 0.3,
-            "confidence_interval": {
-              "lower": 0,
-              "upper": 0
-            }
-          },
-          "success_criterion_assessment": {
-            "conclusion": "Baseline version satisfied all success criteria",
-            "lower_threshold_breached": False,
-            "upper_threshold_breached": False,
-            "probability_of_meeting_success_criterion": 1.0
-          }
+            "id": "simple_ratio",
+            "lower_is_better": True,
+            "numerator_prom_query_template": "sum(of(parts)) > whole",
+            "denomenator_prom_query_template": "whole > sum(or(parts))",
+            "binary_metric": False
+        },
+        {
+            "id": "simple_counter",
+            "lower_is_better": True,
+            "prom_query_template": "count(me_in)"
         }
-      ]
+    ],
+    "assessment_criteria": [
+        {
+            "metric_id": "simple_ratio",
+            "reward": False,
+            "upper_threshold": {
+                "threshold_type": "absolute",
+                "value": 25
+            }
+        }
+    ],
+    "baseline": {
+        "id": "reviews_base",
+        "tags": {
+            'service': "reviews",
+            'deployment': "candid"
+        }
     },
-    {
-      "id": "candidate1",
-      "win_probability": 0.6,
-      "request_count": 1000,
-      "metrics": [
+    "candidates": [
         {
-          "id": "iter8-latency",
-          "statistics": {
-            "sample_size": 1000,
-            "value": 0.002,
-            "improvement": {
-              "lower": 0.24,
-              "upper": 0.46
-            },
-            "probability_to_beat_baseline": 0.0,
-            "probability_to_be_best_version": 0.0,
-            "confidence_interval": {
-              "lower": 0.88,
-              "upper": 0.93
+            "id": "reviews_candidate",
+            "tags": {
+                'service': "reviews",
+                'deployment': "candid"
             }
-          },
-          "success_criterion_assessment": {
-            "conclusion": "Candidate1 version satisfied all success criteria. Candidate2 is the best version",
-            "lower_threshold_breached": False,
-            "upper_threshold_breached": False,
-            "probability_of_meeting_success_criterion": 1.0
-          }
         }
-      ]
+    ],
+    "advanced_traffic_control_parameters": {
+        "exploration_traffic_percentage": 5.0,
+        "check_and_increment_parameters": {
+            "step_size": 1
+        }
     },
-    {
-      "id": "candidate2",
-      "win_probability": 0.3,
-      "request_count": 1100,
-      "metrics": [
-        {
-          "id": "iter8-latency",
-          "statistics": {
-            "sample_size": 1100,
-            "value": 0.004,
-            "improvement": {
-              "lower": 0.10,
-              "upper": 0.12
-            },
-            "probability_to_beat_baseline": 0.0,
-            "probability_to_be_best_version": 0.0,
-            "confidence_interval": {
-              "lower": 0.85,
-              "upper": 0.97
-            }
-          },
-          "success_criterion_assessment": {
-            "conclusion": "Candidate2 version satistied all success criteria.",
-            "lower_threshold_breached": False,
-            "upper_threshold_breached": False,
-            "probability_of_meeting_success_criterion": 1.0
-          }
-        }
-      ]
+    "advanced_assessment_parameters": {
+        "posterior_probability_for_credible_intervals": 95.0,
+        "min_posterior_probability_for_winner": 99.0
     }
-  ],
-  "traffic_split_recommendation": {
-    "recommendation": {
-      "pbr": {
-        "baseline": 10,
-        "candidate1": 60,
-        "candidate": 30
-      },
-      "obr": {
-        "baseline": 5,
-        "candidate1": 80,
-        "candidate2": 15
-      }
-    }},
-    "assessment": {
-    "winning_version_found": False,
-    "winner": "string",
-    "confidence_in_winner": 0,
-    "human_consumable_summary": "string"
+}
+
+ar_example = {
+    'timestamp': "2020-04-03T12:59:50.568Z",
+    'baseline_assessment': {
+        "id": "reviews_base",
+        "request_count": 500,
+        "win_probability": 0.1,
+        "metric_assessments": [
+            {
+                "id": "simple_ratio",
+                "statistics": {
+                    "value": 0.005,
+                    "ratio_statistics": {
+                        "sample_size": 500,
+                        "improvement_over_baseline": {
+                            'lower': 2.3,
+                            'upper': 5.0
+                        },
+                        "probability_of_beating_baseline": .82,
+                        "probability_of_being_best_version": 0.1,
+                        "credible_interval": {
+                            'lower': 22, 
+                            'upper': 28
+                        }
+                    }
+                },
+                "threshold_assessment": {
+                    "lower_threshold_breached": False,
+                    "upper_threshold_breached": False,
+                    "probability_of_satisfying_thresholds": 0.8
+                }
+            }
+        ]
     },
-    "status": 0,
-    "last_state": {}
-  }
-    return resp
+    'candidate_assessments': [
+        {
+            "id": "reviews_candidate",
+            "request_count": 1500,
+            "win_probability": 0.11,
+            "metric_assessments": [
+                {
+                    "id": "simple_ratio",
+                    "statistics": {
+                        "value": 0.1005,
+                        "ratio_statistics": {
+                            "sample_size": 1500,
+                            "improvement_over_baseline": {
+                                'lower': 12.3, 
+                                'upper': 15.0
+                            },
+                            "probability_of_beating_baseline": .182,
+                            "probability_of_being_best_version": 0.1,
+                            "credible_interval": {
+                                'lower': 122, 
+                                'upper': 128
+                            }
+                        }
+                    },
+                    "threshold_assessment": {
+                        "lower_threshold_breached": False,
+                        "upper_threshold_breached": False,
+                        "probability_of_satisfying_thresholds": 0.180
+                    }
+                }
+            ]
+        }
+    ],
+    'traffic_split_recommendation': {
+        'unif': {
+            'reviews_base': 50.0,
+            'reviews_candidate': 50.0
+        }
+    },
+    'winner_assessment': {
+        'winning_version_found': False
+    },
+    'status': ["all_ok"]
+}
+
+@app.post("/assessment", response_model=Iter8AssessmentAndRecommendation)
+def provide_assessment_for_this_experiment_iteration(eip: ExperimentIterationParameters = Body(..., example=eip_example)):
+    """
+      POST iter8 experiment iteration data and obtain assessment of how the versions are performing and recommendations on how to split traffic based on multiple strategies.
+      """
+
+    return Iter8AssessmentAndRecommendation(** ar_example)
--- iter8_analytics/api/analytics/request_parameters_hil.py
+++ iter8_analytics/api/analytics/request_parameters_hil.py
@@ -1,137 +1,104 @@
 """
 Specification of the request parameters for the REST API code related to analytics.
 """
-
-from pydantic import BaseModel
+# Core python stuff
 from datetime import datetime
 from enum import Enum
-from typing import List, Tuple
-####
-# Schema of the request body with the parameters for
-# POST /experiment/<algorithm-name>
-####
+from typing import Tuple, Union, Sequence
 
-#Questions/Comments:
-# 1. How do I set example values that will be available to the user in Swagger UI
-# 2. Reward Criterion is same as Criterion
-# 3. Go over the comments on the git issue
-# 4. Confidence gt, lt values?
-# 5. Named tuples for belief distributions has been removed
-# 6. Should I validate Last State or let it be a raw dictionary as was the case in our prevous design?
+# Module dependencies
+from pydantic import BaseModel, Field
 
-class VersionDefinition(BaseModel):
-    """
-    Create an item with all the information:
+# iter8 stuff
+from iter8_analytics.api.analytics.response_hil import Iter8AssessmentAndRecommendation
 
-    - **start_time**: ISO8601 timestamp for the beginning of the time range of interest
-    - **end_time**: ISO8601 timestamp for the end of the time range of interest; if omitted, current time is assumed
-    - **tags**: Key-value pairs identifying the data pertaining to a version
-    """
-    start_time: datetime = Field(..., description="ISO8601 timestamp for the beginning of the time range of interest")
-    end_time: datetime = Field(None, description="ISO8601 timestamp for the end of the time range of interest; if omitted, current time is assumed")
-    tags: dict = Field(..., description="Key-value pairs identifying the data pertaining to a version")
-
-class MinMax(BaseModel):
-    """
-    - **min**: Minimum possible value of the metric
-    - **max**: Maximum possible value of the metric
-    """
-    min: float = Field(..., description="Minimum possible value of the metric")
-    max: float = Field(..., description="Maximum possible value of the metric")
-
-class CriterionEnum(str, Enum):
-    delta = 'delta'
-    threshold = 'threshold'
-
-class Criterion(BaseModel):
-    """
-    - **metric_name**: Name of the metric to which the criterion applies
-    - **is_counter**: Describles the type of metric. Options: "True": Metrics which are cumulative in nature and represent monotonically increasing values
-    - **absent_value**: Describes what value should be returned if Prometheus did not find any data corresponding to the metric
-    - **metric_query_template**: Prometheus Query of the metric to which the criterion applies
-    - **metric_sample_size_query_template**: Sample Size Query for the metric to which the criterion applies
-    - **min_max**: The minimum and maximum possible value of the metric, if available
-    """
-    metric_name: str = Field(..., description='Name of the metric to which the criterion applies')
-    is_counter: bool = Field(..., description='Describles the type of metric. Options: "True": Metrics which are cumulative in nature and represent monotonically increasing values')
-    absent_value: str = Field(None, description='Describes what value should be returned if Prometheus did not find any data corresponding to the metric')
-    metric_query_template: str = Field(..., description='Prometheus Query of the metric to which the criterion applies')
-    metric_sample_size_query_template: str = Field(..., description='Sample Size Query for the metric to which the criterion applies')
-    min_max: MinMax = None = Field(None, description='The minimum and maximum possible value of the metric, if available')
-
-class SuccessCriterion(Criterion):
-    """
-    - **type**: Criterion type. Options: "delta": compares the candidate against the baseline version with respect to the metric; "threshold": checks the candidate with respect to the metric
-    - **value**: Value to be check against
-    - **stop_on_failure**: Indicates whether or not the experiment must finish if this criterion is not satisfied; defaults to false
-    """
-    type: CriterionEnum = Field(..., description='Criterion type. Options: "delta": compares '
-        'the candidate against the baseline version with respect to the metric; "threshold": '
-        'checks the candidate with respect to the metric')
-    value: float = Field(..., description='Value to be check against')
-    stop_on_failure: bool = Field(False, description='Indicates whether or not the experiment '
-        'must finish if this criterion is not satisfied; defaults to false')
+class Version(BaseModel):
+    id: str = Field(..., description="ID of the version")
+    tags: dict = Field(..., description="Key-value pairs used in prometheus queries to enable version level grouping of metrics")
 
 
-class TrafficControlDefault(BaseModel):
-    max_traffic_percentage: float = Field(50, description='Maximum percentage of traffic that the candidate version '
-        'will receive during the experiment; defaults to 50%', gt=0.0)
-    success_criteria: List[SuccessCriterion] = Field(..., description='List of criteria for assessing the candidate version',
-        min_items=1)
-    reward: Criterion = Field(None, description="Reward attribute to maximize in the A/B test")
-
-class TrafficControlCheckAndIncrement(TrafficControlDefault):
-    step_size: float = Field(2.0, description='Increment (in percent points) to be applied to the '
-        'traffic received by the candidate version each time it passes the '
-        'success criteria; defaults to 1 percent point', gt=1.0)
+class Iter8Metric(BaseModel):
+    id: str = Field(..., description="ID of the metric")
+    lower_is_better: bool = Field(
+        ..., description="Boolean flag indicating if lower values of this metric are preferable to higher values")
 
 
-class TrafficControlBayesianRouting(TrafficControlDefault):
-    confidence: float = Field(0.95, gt=0.0, lt=1, description='Posterior probability that all success criteria is met')
-    reward: Criterion = Field(None, description='Reward attribute to maximize in the A/B test')
-
-class VersionLastStateDefault(BaseModel):
-    traffic_percentage: float = Field(..., description="Percentage of traffic to this version")
-    success_criterion: List[tuple] = Field(..., description='List of success criterion results from the previous iteration '
-        'for this version')
-
-class VersionLastStateBayesianRouting(BaseModel):
-    success_criterion_belief: List[Tuple[float, float, float, float]] = Field(..., description='The belief distribution of each '
-        'success criterion metric')
-    reward_belief: Tuple[float, float, float, float] = Field(..., description="The belief distribution of the reward metric")
-
-class LastStateDefault(BaseModel):
-    baseline: VersionLastStateDefault = Field({"traffic_percentage": 100, "success_criterion": []}, description='Baseline traffic and successcriterion information for the '
-        'previous iteration')
-    candidate: VersionLastStateDefault = Field({"traffic_percentage": 0, "success_criterion": []}, description='Candidate traffic and successcriterion information for the '
-        'previous iteration')
-    change_observed: bool = Field(False, description='Will always take False at the beginning of the iteration')
+# counter metric defined in iter8 configmaps
+class Iter8CounterMetric(Iter8Metric):
+    prom_query_template: str = Field(...,
+                                     description="Prometheus query template")
 
 
-class LastStateEpsilonTGreedy(LastStateDefault):
-    effective_iteration_count: int = Field(0, description='Count of the number of times traffic was updated '
-        'in this experiment')
+class Iter8RatioMetric(Iter8Metric):  # ratio metric defined in iter8 configmaps
+    numerator_prom_query_template: str = Field(
+        ..., description="Prometheus query template for numerator")
+    denomenator_prom_query_template: str = Field(
+        ..., description="Prometheus query template for numerator")
+    binary_metric: bool = Field(
+        False, description="This metric is mean value of a binary variable")
 
-class LastStateBayesianRouting(BaseModel):
-    baseline: VersionLastStateBayesianRouting = Field({"success_criterion_belief": [], "reward_belief": [None, None, None, None]})
-    candidate: VersionLastStateBayesianRouting = Field({"success_criterion_belief": [], "reward_belief": [None, None, None, None]})
+
+class ThresholdEnum(str, Enum):
+    absolute = "absolute"  # this threshold represents an absolute limit
+    relative = "relative"  # this threshold represents a limit relative to baseline
 
 
-class RequestParametersDefault(BaseModel):
-    baseline: VersionDefinition = Field(..., description='Specifies a time interval and key-value pairs for '
-        'retrieving and processing data pertaining to the baseline version')
-    candidate: VersionDefinition = Field(..., description='Specifies a time interval and key-value pairs for '
-        'retrieving and processing data pertaining to the candidate version')
+class Threshold(BaseModel):
+    threshold_type: ThresholdEnum = Field(..., description="Type of threshold")
+    value: float = Field(..., description="Value of threshold")
 
-class RequestParametersCheckAndIncrement(RequestParametersDefault):
 
-    traffic_control: TrafficControlCheckAndIncrement = Field(..., description='Parameters controlling the behavior of the analytics')
-    last_state: LastStateDefault = Field(..., description='State returned by the server on the previous call')
+class AssessmentCriterion(BaseModel):
+    metric_id: str = Field(
+        ..., description="ID of the metric. This matches the unique ID of the metric in iter8 metric definition")
+    reward: bool = Field(
+        False, description="Boolean flag indicating if this metric will be used as reward to be optimized in an A/B test. Only ratio metrics can be used as a reward. At most one metric can be used as reward")
+    lower_threshold: Threshold = Field(
+        None, description="Lower threshold for this metric")
+    upper_threshold: Threshold = Field(
+        None, description="Upper threshold for this metric")
 
-class RequestParametersEpsilonTGreedy(RequestParametersDefault):
-    traffic_control: TrafficControlDefault = Field(..., description='Parameters controlling the behavior of the analytics')
-    last_state: LastStateEpsilonTGreedy = Field(..., description='State returned by the server on the previous call')
 
-class RequestParametersBayesianRouting(RequestParametersDefault):
-    traffic_control: TrafficControlBayesianRouting = Field(..., description='Parameters controlling the behavior of the analytics')
-    last_state: LastStateBayesianRouting = Field(..., description='State returned by the server on the previous call')
+class TrafficControlStrategy(str, Enum):
+    uniform = "Uniform"
+    check_and_increment = "Check and Increment"
+    epsilon_t_greedy = "Decaying Epsilon Greedy"
+    pbr = "Posterior Bayesian Routing"
+    top_k_pbr = "Top-k Posterior Bayesian Routing"
+
+
+class CheckAndIncrementParameters(BaseModel):
+    step_size: float = Field(
+        1.0, description="Minimum possible traffic increment in check and  increment")
+
+
+class AdvancedTrafficControlParameters(BaseModel):
+    exploration_traffic_percentage: float = Field(
+        5.0, description="Percentage of traffic used for exploration", ge=0.0, le=100.0)
+    check_and_increment_parameters: CheckAndIncrementParameters = Field(
+        None, description="Parameters for check and increment strategy")
+
+
+class AdvancedAssessmentParameters(BaseModel):
+    posterior_probability_for_credible_intervals: float = Field(
+        95.0, description="Posterior probability used for computing credible intervals in assessment")
+    min_posterior_probability_for_winner: float = Field(
+        99.0, description="Minimum value of posterior probability of being the best version which needs to be attained by a version to be declared winner")
+
+# parameters for current iteration of experiment
+class ExperimentIterationParameters(BaseModel):
+    start_time: datetime = Field(...,
+                                 description="Start time of the experiment")
+    iter8_metrics: Sequence[Union[Iter8RatioMetric, Iter8CounterMetric]] = Field(
+        ..., description="All iter8 metric definitions. Includes ratio and counter metrics")
+    assessment_criteria: Sequence[AssessmentCriterion] = Field(
+        ..., description="Specifications for metric based criteria to be assessed in this experiment")
+    baseline: Version = Field(..., description="The baseline version")
+    candidates: Sequence[Version] = Field(...,
+                                          description="The sequence of candidates")
+    advanced_traffic_control_parameters: AdvancedTrafficControlParameters = Field(
+        None, description="Advanced traffic control related parameters")
+    advanced_assessment_parameters: AdvancedAssessmentParameters = Field(
+        None, description="Advanced assessment related parameters")
+    last_state: Iter8AssessmentAndRecommendation = Field(
+        None, description="Last recorded state (response) from analytics service")
