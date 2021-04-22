da266ae890908bf1aa793a7f5acfa006cf08c20c
Srinivasan Parthasarathy <5309125+sriumcp@users.noreply.github.com>
2021-04-22 15:43:28 -0400

    Unit tests for sysdig token embedded in secret

--- iter8_analytics/api/v2/examples/examples_metrics.py
+++ iter8_analytics/api/v2/examples/examples_metrics.py
@@ -195,3 +195,32 @@ sysdig_embedded = {
     "urlTemplate": "https://secure.sysdig.com/api/data"
   }
 }
+
+sysdig_secret = {
+  "apiVersion": "iter8.tools/v2alpha2",
+  "kind": "Metric",
+  "metadata": {
+    "name": "cpu-utilization"
+  },
+  "spec": {
+    "description": "A Sysdig example",
+    "provider": "sysdig",
+    "body": "{\n  \"last\": ${elapsedTime},\n  \"sampling\": 600,\n  \"filter\": \"kubernetes.app.revision.name = '${revision}'\",\n  \"metrics\": [\n    {\n      \"id\": \"cpu.cores.used\",\n      \"aggregations\": { \"time\": \"avg\", \"group\": \"sum\" }\n    }\n  ],\n  \"dataSourceType\": \"container\",\n  \"paging\": {\n    \"from\": 0,\n    \"to\": 99\n  }\n}",
+    "method": "POST",
+    "authType": "Bearer",
+    "secret": "myns/sdcredentials",
+    "type": "Gauge",
+    "headerTemplates": [
+      {
+        "name": "Accept",
+        "value": "application/json"
+      },
+      {
+        "name": "Authorization",
+        "value": "Bearer ${token}"
+      }
+    ],
+    "jqExpression": ".data[0].d[0] | tonumber",
+    "urlTemplate": "https://secure.sysdig.com/api/data"
+  }
+}
